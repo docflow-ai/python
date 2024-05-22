f93c494abba2675c5fdc3e88be478051d7e47e97
wacco <branislav@vaculciak.sk>
2024-05-22 10:30:23 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,22 +8,18 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires='>=3.9',
-    install_requires=['mpi4py>=2.0',
-                      'numpy',
-                      'unidecode',
-                      'shapely',
-                      'requests',
-                      'Pillow',
-                      'validators',
-                      'pyOpenSSL',
-                      'pymongo',
-                      'geotext',
-                      'google-auth',
-                      'google-cloud-vision',
-                      'google-cloud-storage',
-                      'bpemb',
-                      'attrdict',
-                      'lxml',
-                      'dateparser'
-                      ]
+    install_requires=[
+        'mpi4py>=2.0',
+        'bson',
+        'certifi',
+        'chardet',
+        'filetype',
+        'idna',
+        'PyPDF2',
+        'python-dateutil',
+        'pytz',
+        'requests',
+        'six',
+        'urllib3'
+    ]
 )
